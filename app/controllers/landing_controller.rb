8d5e44041ef86a02a73fce5b7db12ecc3200c60d
WaiZack <a0111636@u.nus.edu>
2017-03-11 17:55:22 +0800

    commit before pull

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,7 @@
 class LandingController < ApplicationController
 
   def home
+    @mailing_list = MailingList.new
   end
 
 end
---
+++ test/controllers/mailing_list_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MailingListControllerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
