2bd3261a81bedcd1fab139e058f2c946106c432a
WaiZack <a0111636@u.nus.edu>
2017-03-25 10:47:24 +0800

    Team member number limit control

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -4,11 +4,10 @@ class AccountActivationController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
-      log_in user
-      flash.now[:success] = "Account activated!"
+      flash[:success] = "Account activated!"
       redirect_to '/login'
     else
-      flash.now[:danger] = "Invalid activation link"
+      flash[:danger] = "Invalid activation link"
       redirect_to '/login'
     end
   end
