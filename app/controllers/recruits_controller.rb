ba27e3319362fc1caeee05f55b44867d3611f526
WaiZack <a0111636@u.nus.edu>
2017-05-17 08:27:59 +0800

    removed mailinglist

--- app/controllers/recruits_controller.rb
+++ app/controllers/recruits_controller.rb
@@ -1,12 +1,13 @@
 class RecruitsController< ApplicationController
 
+  before_action :logged_in_user
+
   def logged_in_user
     unless logged_in?
       flash[:danger] = 'You are not logged in'
       redirect_to login_url
     end
   end
-
 
   def show
     @recruit = Recruit.all
