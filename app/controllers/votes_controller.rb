aba04916ec87dd6ea96793ca9f09971ebf67a72b
WaiZack <a0111636@u.nus.edu>
2017-06-17 12:20:05 +0800

    sort alpha

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,12 +2,12 @@ class VotesController < ApplicationController
 
   def new
     @vote = Vote.new
-    @teams = Team.pluck(:name)
+    @teams = Team.pluck(:name).sort_by!{|e| e.downcase}
   end
 
   def create
     @vote = Vote.new(votes_param)
-    @teams = Team.pluck(:name)
+    @teams = Team.pluck(:name).sort_by!{|e| e.downcase}
     if @vote.save
       redirect_to '/votingDone'
     else
