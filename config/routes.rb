327ed3374a87d3e8d44ad796a6dc17514b42bd49
WaiZack <a0111636@u.nus.edu>
2017-07-07 14:25:06 +0800

    turn off portal

--- config/routes.rb
+++ config/routes.rb
@@ -30,60 +30,60 @@ Rails.application.routes.draw do
 
 
 
-  get '/dash' => 'dashboard#edit'
-  get '/users' => 'users#new'
-  get  '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-
-  #new routes
-  # get '/login', to: 'mailing_list#index'
-  # get '/hellogoodbye', to: 'sessions#new'
-  # get '/goodbyehello' => 'users#new'
-  #end new routes
-
-
-  delete '/logout',  to: 'sessions#destroy'
-
-  get '/changePassword' => 'dashboard#edit'
-
-  get '/teams' => 'teams#index'
-  # get '/createTeam' => 'teams#new'
-  get '/myTeams' => 'teams#myteams'
-
-  get '/approve' => 'membership_requests#approve'
-  get '/reject' => 'membership_requests#reject'
-  get '/removeMember' => 'teams#remove'
-  get '/quitTeam' => 'teams#quit'
-  get '/deleteTeam' => 'teams#delete'
-  post '/addList' => 'mailing_list#new'
-  get '/profile' => 'dashboard#show'
-  get '/password_back' => 'password_resets#back'
-  get '/recruit' => 'recruits#show'
-  get '/newRecruit' => 'recruits#new'
-  get '/recruited' => 'recruits#delete'
-  get '/NDAnotNBA' => 'ndas#new'
-  get '/volunteers' => 'slaves#new'
-  get '/volunteerDone' => 'slaves#done'
-  post '/volunteering' => 'slaves#create'
-
-  post '/addList2' => 'mailing_list#new2'
+  # get '/dash' => 'dashboard#edit'
+  # get '/users' => 'users#new'
+  # get  '/login',   to: 'sessions#new'
+  # post   '/login',   to: 'sessions#create'
+  #
+  # #new routes
+  # # get '/login', to: 'mailing_list#index'
+  # # get '/hellogoodbye', to: 'sessions#new'
+  # # get '/goodbyehello' => 'users#new'
+  # #end new routes
+  #
+  #
+  # delete '/logout',  to: 'sessions#destroy'
+  #
+  # get '/changePassword' => 'dashboard#edit'
+  #
+  # get '/teams' => 'teams#index'
+  # # get '/createTeam' => 'teams#new'
+  # get '/myTeams' => 'teams#myteams'
+  #
+  # get '/approve' => 'membership_requests#approve'
+  # get '/reject' => 'membership_requests#reject'
+  # get '/removeMember' => 'teams#remove'
+  # get '/quitTeam' => 'teams#quit'
+  # get '/deleteTeam' => 'teams#delete'
+  # post '/addList' => 'mailing_list#new'
+  # get '/profile' => 'dashboard#show'
+  # get '/password_back' => 'password_resets#back'
+  # get '/recruit' => 'recruits#show'
+  # get '/newRecruit' => 'recruits#new'
+  # get '/recruited' => 'recruits#delete'
+  # get '/NDAnotNBA' => 'ndas#new'
+  # get '/volunteers' => 'slaves#new'
+  # get '/volunteerDone' => 'slaves#done'
+  # post '/volunteering' => 'slaves#create'
+  #
+  # post '/addList2' => 'mailing_list#new2'
 
   # get '/TazsBrovhCJgKHYibYCE' => 'votes#new'
 
   # get 'votingDone' => 'votes#done'
 
 
-  resources :users, except: [:new]
-  resources :recruits
-  resources :dashboard
-  resources :account_activation, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update, :back]
-  resources :teams
-  # resources :membership_requests
-  resources :mailing_list
-  resources :submissions, only: [:create, :update]
-  resources :ndas
-  resources :slaves
-  resources :votes
+  # resources :users, except: [:new]
+  # resources :recruits
+  # resources :dashboard
+  # resources :account_activation, only: [:edit]
+  # resources :password_resets,     only: [:new, :create, :edit, :update, :back]
+  # resources :teams
+  # # resources :membership_requests
+  # resources :mailing_list
+  # resources :submissions, only: [:create, :update]
+  # resources :ndas
+  # resources :slaves
+  # resources :votes
 
 end
